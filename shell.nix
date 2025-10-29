fff1dc166038534db6bf4077c5fa491fd4b7253b
Amar Mehta <xfthhxk@gmail.com>
2025-10-29 18:39:21 +0000

    upgrade to java 25

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ use_zsh ? "true" }:
+let
+  # as of 28 Oct 2025. See: https://status.nixos.org
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/01f116e4df6a15f4ccdffb1bcd41096869fb385c.tar.gz";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  packages = [
+   pkgs.babashka
+   pkgs.docker
+   pkgs.docker-buildx
+   pkgs.zsh
+  ];
+
+  shellHook = ''
+    if [[ ${use_zsh} = true ]]
+    then
+      exec zsh
+    fi
+  '';
+}
